83a78a1dbda6831473d5458550d10b0f2231e821
daanii999 <iron.cfg@gmail.com>
2022-02-03 16:38:20 +0100

    yii2 basic framework

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=teszt_feladat',
+    'username' => 'teszt_feladat',
+    'password' => 'atsALFbf/DPcM-gQ',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
